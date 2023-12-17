bc658d513d8c68d42b408d3f59c8d0156f59631c
Ziyu Huang <ziyu4huang@gmail.com>
2023-12-17 20:01:06 +0800

    up

--- qt4_dev/default.nix
+++ qt4_dev/default.nix
@@ -1,7 +1,7 @@
 # before use run
-# nixpkgs https://channels.nixos.org/nixos-23.11
-# nix-channel --add https://channels.nixos.org/nixos-23.05 nixpkgs-23.05
 # https://nixos.wiki/wiki/Development_environment_with_nix-shell
+#
+# nix-channel --add https://channels.nixos.org/nixos-23.05 nixpkgs-23.05
 # nix-channel --update
 
 { pkgs ? import <nixpkgs-23.05> {} }:
@@ -10,18 +10,19 @@
     # nativeBuildInputs is usually what you want -- tools you need to run
     nativeBuildInputs = with pkgs.buildPackages; [ 
       gnumake git cmake neovim
-      bison flex
-      gcc11 bazel
-      openmpi openssl
+      bison flex gcc11
+      openssl gperftools
+      patchelf
 
       tcl tcllib tclreadline tk readline
       eigen
-      boost 
+      boost
       qt4
       ];
     shellHook = ''
       alias ll="ls -l"
       export FOO=bar
+      export PROMPT='<maxdev>~$'
     '';
 }
 
